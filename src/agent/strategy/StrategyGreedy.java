148e93c3ea0b85a8e391bff7c445adbc321f5a94
ThomasArnaud <thomas.arnaud@etu.univ-lyon1.fr>
2016-03-30 08:05:26 +0200

    add GreedyStrategy code

--- src/agent/strategy/StrategyGreedy.java
+++ src/agent/strategy/StrategyGreedy.java
@@ -6,6 +6,8 @@ import java.util.Random;
 import agent.rlagent.RLAgent;
 import environnement.Action;
 import environnement.Etat;
+import environnement.gridworld.ActionGridworld;
+import java.util.ArrayList;
 
 /**
  * Strategie qui renvoit une action aleatoire avec probabilite epsilon, une
@@ -16,30 +18,45 @@ import environnement.Etat;
  *
  */
 public class StrategyGreedy extends StrategyExploration {
-    //TODO
-    //...
+    protected double epsilon;
 
     private Random rand = new Random();
 
     public StrategyGreedy(RLAgent agent, double epsilon) {
         super(agent);
-        //TODO
-        //...
+        this.epsilon = epsilon;
     }
 
     /**
+     * @param _e
      * @return action selectionnee par la strategie d'exploration
      */
     @Override
     public Action getAction(Etat _e) {
-        //VOTRE CODE
         //getAction renvoi null si _e absorbant
-
-        return null;
+        if (this.agent.getEnv().estAbsorbant()) {
+            return null;
+        }
+        
+        if (rand.nextDouble() < this.epsilon) {
+            List<Action> allActions = new ArrayList<>();
+            allActions.add(ActionGridworld.NORD);
+            allActions.add(ActionGridworld.SUD);
+            allActions.add(ActionGridworld.EST);
+            allActions.add(ActionGridworld.OUEST);
+            
+            return allActions.get(rand.nextInt(4));
+        } else {
+            List<Action> politique = this.agent.getPolitique(_e);
+            if ( politique.size() > 0) {
+                return politique.get(0);
+            } else {
+                return null;
+            }
+        }
     }
 
     public void setEpsilon(double epsilon) {
-        //VOTRE CODE
-
+        this.epsilon = epsilon;
     }
 }
